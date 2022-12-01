5cb4a5fbd8a9c637253e4c7b0645a063ce6de6a9
aQaTL <mmsoltys@outlook.com>
2022-12-01 06:13:58 +0100

    Day 1

--- day_1/src/main.rs
+++ day_1/src/main.rs
@@ -1,14 +1,37 @@
+use anyhow::anyhow;
+use itertools::Itertools;
+
 fn main() -> anyhow::Result<()> {
 	let input = std::fs::read_to_string("day_1/input.txt")?;
 	println!("Part 1: {}", part_1(&input)?);
-	// println!("Part 2: {}", part_2(&input)?);
+	println!("Part 2: {}", part_2(&input)?);
 	Ok(())
 }
 
 fn part_1(input: &str) -> anyhow::Result<u64> {
-	todo!()
+	input
+		.split("\n\n")
+		.map(|elf| {
+			elf.split('\n')
+				.filter_map(|num| num.parse::<u64>().ok())
+				.sum()
+		})
+		.max()
+		.ok_or_else(|| anyhow!("malformed input"))
 }
 
 fn part_2(input: &str) -> anyhow::Result<u64> {
-	todo!()
+	let top_three_sum = input
+		.split("\n\n")
+		.map(|elf| {
+			elf.split('\n')
+				.filter_map(|num| num.parse::<u64>().ok())
+				.sum::<u64>()
+		})
+		.sorted()
+		.rev()
+		.take(3)
+		.sum();
+
+	Ok(top_three_sum)
 }
